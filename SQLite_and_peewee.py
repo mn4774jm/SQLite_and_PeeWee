2f67bd207bc9d64b1437fafa7b4b7220219eec26
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-09-22 09:06:05 -0500

    created model class and connected database

--- SQLite_and_peewee.py
+++ SQLite_and_peewee.py
@@ -1,2 +1,17 @@
 from peewee import *
 
+db = SqliteDatabase('records.sqlite')
+
+# Model class created to produce columns in database
+class Record(Model):
+    name = CharField()
+    country = CharField()
+    catches = IntegerField()
+
+# connected to database
+    class Meta:
+        database = db
+
+# set string return statement
+    def __str__(self):
+        return f'Name: {self.name} | Country: {self.country} | Catches: {self.catches}'
